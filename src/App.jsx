023dbafcb33613291ab562a6d8ecb1b1df9f587c
Marta Organisciak <marta.organisciak@wp.pl>
2023-11-18 19:35:25 +0100

    tailwind added

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,15 @@ import React from "react";
 import "./App.css";
 import VideoPlayer from "./components/VideoPlayer";
 
-// import Footer from "./components/Footer";
-
 function App() {
-  const [count, setCount] = useState(0);
-
-  const videoUrl =
-    "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4";
-
   return (
     <>
-      <div className="App">
-        {/* <VideoPicture /> */}
-        <div className="videoFrame">
-          <div id="video">
-            <VideoPlayer videoUrl={videoUrl} />
-          </div>
-        </div>
-        <div className="footer">{/* <Footer /> */}</div>
+      <div className="h-full w-full bg-black">
+        {/* <Header/> */}
+
+        <VideoPlayer />
+
+        {/* <Footer /> */}
       </div>
     </>
   );
--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -1,61 +1,13 @@
 import React, { useRef, useState } from "react";
-import ReactPlayer from "react-player";
 
-const VideoPlayer = ({ videoUrl }) => {
-  const playerRef = useRef(null);
-  const [isPlaying, setIsPlaying] = useState(false);
-  const [progress, setProgress] = useState(0);
-  const [isMuted, setIsMuted] = useState(false);
-
-  const handlePlayPause = () => {
-    setIsPlaying(!isPlaying);
-  };
-
-  const handleMuteUnmute = () => {
-    setIsMuted(!isMuted);
-  };
-
-  const handleProgress = (state) => {
-    setProgress(state.played);
-  };
-
-  const handleSeek = (percentage) => {
-    playerRef.current.seekTo(percentage);
-  };
-
+export default function VideoPlayer() {
   return (
-    <>
-      <div className="video-player">
-        <ReactPlayer
-          ref={playerRef}
-          url={videoUrl}
-          playing={isPlaying}
-          muted={isMuted}
-          onProgress={handleProgress}
-          controls={false}
-        />
-        <div className="controls">
-          {/* //play oder pause */}
-          {/* <button onClick={handlePlayPause}>{isPlaying}</button> */}
-          <button onClick={handlePlayPause}>{isPlaying ? "||" : "Play"}</button>
-
-          {/* //laut oder mute */}
-          <button onClick={handleMuteUnmute}>
-            {isMuted ? "Unmute" : "Mute"}
-          </button>
-          <input
-            id="progress"
-            type="range"
-            min={0}
-            max={1}
-            step="any"
-            value={progress}
-            onChange={(e) => handleSeek(parseFloat(e.target.value))}
-          />
-        </div>
-      </div>
-    </>
+    <video className="h-full w-full rounded-lg" controls>
+      <source
+        src="https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4"
+        type="video/mp4"
+      />
+      Your browser does not support the video tag.
+    </video>
   );
-};
-
-export default VideoPlayer;
+}
