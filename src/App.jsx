ad27c53756cac46b7d1b0b167202cfd0542fbb8d
Marta Organisciak <marta.organisciak@wp.pl>
2023-11-20 15:40:21 +0100

    header

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import { useState } from "react";
 import React from "react";
 import "./App.css";
 import VideoPlayer from "./components/VideoPlayer";
+import Header from "./components/Header";
 
 function App() {
   return (
     <>
       <div className="h-full w-full bg-black">
-        {/* <Header/> */}
+        <Header />
 
         <VideoPlayer />
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,434 @@
+import React from "react";
+import {
+  Navbar,
+  MobileNav,
+  Typography,
+  Button,
+  IconButton,
+  Card,
+} from "@material-tailwind/react";
+
+export default function Header() {
+  const [openNav, setOpenNav] = React.useState(false);
+
+  React.useEffect(() => {
+    window.addEventListener(
+      "resize",
+      () => window.innerWidth >= 960 && setOpenNav(false)
+    );
+  }, []);
+
+  const navList = (
+    <ul className="mt-2 mb-4 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
+      <Typography
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-1 font-normal"
+      >
+        <a href="#" className="flex items-center">
+          Pages
+        </a>
+      </Typography>
+      <Typography
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-1 font-normal"
+      >
+        <a href="#" className="flex items-center">
+          Account
+        </a>
+      </Typography>
+      <Typography
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-1 font-normal"
+      >
+        <a href="#" className="flex items-center">
+          Blocks
+        </a>
+      </Typography>
+      <Typography
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-1 font-normal"
+      >
+        <a href="#" className="flex items-center">
+          Docs
+        </a>
+      </Typography>
+    </ul>
+  );
+
+  return (
+    <div className="-m-6 max-h-[768px] w-[calc(100%+48px)] overflow-scroll">
+      <Navbar className="sticky top-0 z-10 h-max max-w-full rounded-none px-4 py-2 lg:px-8 lg:py-4">
+        <div className="flex items-center justify-between text-blue-gray-900">
+          <Typography
+            as="a"
+            href="#"
+            className="mr-4 cursor-pointer py-1.5 font-medium"
+          >
+            Material Tailwind
+          </Typography>
+          <div className="flex items-center gap-4">
+            <div className="mr-4 hidden lg:block">{navList}</div>
+            <div className="flex items-center gap-x-1">
+              <Button
+                variant="text"
+                size="sm"
+                className="hidden lg:inline-block"
+              >
+                <span>Log In</span>
+              </Button>
+              <Button
+                variant="gradient"
+                size="sm"
+                className="hidden lg:inline-block"
+              >
+                <span>Sign in</span>
+              </Button>
+            </div>
+            <IconButton
+              variant="text"
+              className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
+              ripple={false}
+              onClick={() => setOpenNav(!openNav)}
+            >
+              {openNav ? (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  className="h-6 w-6"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              ) : (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M4 6h16M4 12h16M4 18h16"
+                  />
+                </svg>
+              )}
+            </IconButton>
+          </div>
+        </div>
+        <MobileNav open={openNav}>
+          {navList}
+          <div className="flex items-center gap-x-1">
+            <Button fullWidth variant="text" size="sm" className="">
+              <span>Log In</span>
+            </Button>
+            <Button fullWidth variant="gradient" size="sm" className="">
+              <span>Sign in</span>
+            </Button>
+          </div>
+        </MobileNav>
+      </Navbar>
+      <div className="mx-auto max-w-screen-md py-12">
+        <Card className="mb-12 overflow-hidden">
+          <img
+            alt="nature"
+            className="h-[32rem] w-full object-cover object-center"
+            src="https://images.unsplash.com/photo-1485470733090-0aae1788d5af?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2717&q=80"
+          />
+        </Card>
+        <Typography variant="h2" color="blue-gray" className="mb-2">
+          What is Material Tailwind
+        </Typography>
+        <Typography color="gray" className="font-normal">
+          Can you help me out? you will get a lot of free exposure doing this
+          can my website be in english?. There is too much white space do less
+          with more, so that will be a conversation piece can you rework to make
+          the pizza look more delicious other agencies charge much lesser can
+          you make the blue bluer?. I think we need to start from scratch can my
+          website be in english?, yet make it sexy i&apos;ll pay you in a week
+          we don&apos;t need to pay upfront i hope you understand can you make
+          it stand out more?. Make the font bigger can you help me out? you will
+          get a lot of free exposure doing this that&apos;s going to be a chunk
+          of change other agencies charge much lesser. Are you busy this
+          weekend? I have a new project with a tight deadline that&apos;s going
+          to be a chunk of change. There are more projects lined up charge extra
+          the next time.
+        </Typography>
+      </div>
+    </div>
+  );
+}
+
+// import { Fragment, useState } from "react";
+// import { Dialog, Disclosure, Popover, Transition } from "@headlessui/react";
+// import {
+//   ArrowPathIcon,
+//   Bars3Icon,
+//   ChartPieIcon,
+//   CursorArrowRaysIcon,
+//   FingerPrintIcon,
+//   SquaresPlusIcon,
+//   XMarkIcon,
+// } from "@heroicons/react/24/outline";
+// import {
+//   ChevronDownIcon,
+//   PhoneIcon,
+//   PlayCircleIcon,
+// } from "@heroicons/react/20/solid";
+
+// const products = [
+//   {
+//     name: "Analytics",
+//     description: "Get a better understanding of your traffic",
+//     href: "#",
+//     icon: ChartPieIcon,
+//   },
+//   {
+//     name: "Engagement",
+//     description: "Speak directly to your customers",
+//     href: "#",
+//     icon: CursorArrowRaysIcon,
+//   },
+//   {
+//     name: "Security",
+//     description: "Your customers’ data will be safe and secure",
+//     href: "#",
+//     icon: FingerPrintIcon,
+//   },
+//   {
+//     name: "Integrations",
+//     description: "Connect with third-party tools",
+//     href: "#",
+//     icon: SquaresPlusIcon,
+//   },
+//   {
+//     name: "Automations",
+//     description: "Build strategic funnels that will convert",
+//     href: "#",
+//     icon: ArrowPathIcon,
+//   },
+// ];
+// const callsToAction = [
+//   { name: "Watch demo", href: "#", icon: PlayCircleIcon },
+//   { name: "Contact sales", href: "#", icon: PhoneIcon },
+// ];
+
+// function classNames(...classes) {
+//   return classes.filter(Boolean).join(" ");
+// }
+
+// export default function Header() {
+//   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+//   return (
+//     <header className="bg-white">
+//       <nav
+//         className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
+//         aria-label="Global"
+//       >
+//         <div className="flex lg:flex-1">
+//           <a href="#" className="-m-1.5 p-1.5">
+//             <span className="sr-only">Your Company</span>
+//             <img
+//               className="h-8 w-auto"
+//               src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+//               alt=""
+//             />
+//           </a>
+//         </div>
+//         <div className="flex lg:hidden">
+//           <button
+//             type="button"
+//             className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+//             onClick={() => setMobileMenuOpen(true)}
+//           >
+//             <span className="sr-only">Open main menu</span>
+//             <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+//           </button>
+//         </div>
+//         <Popover.Group className="hidden lg:flex lg:gap-x-12">
+//           <Popover className="relative">
+//             <Popover.Button className="flex items-center gap-x-1 text-sm font-semibold leading-6 text-gray-900">
+//               Product
+//               <ChevronDownIcon
+//                 className="h-5 w-5 flex-none text-gray-400"
+//                 aria-hidden="true"
+//               />
+//             </Popover.Button>
+
+//             <Transition
+//               as={Fragment}
+//               enter="transition ease-out duration-200"
+//               enterFrom="opacity-0 translate-y-1"
+//               enterTo="opacity-100 translate-y-0"
+//               leave="transition ease-in duration-150"
+//               leaveFrom="opacity-100 translate-y-0"
+//               leaveTo="opacity-0 translate-y-1"
+//             >
+//               <Popover.Panel className="absolute -left-8 top-full z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl bg-white shadow-lg ring-1 ring-gray-900/5">
+//                 <div className="p-4">
+//                   {products.map((item) => (
+//                     <div
+//                       key={item.name}
+//                       className="group relative flex items-center gap-x-6 rounded-lg p-4 text-sm leading-6 hover:bg-gray-50"
+//                     >
+//                       <div className="flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
+//                         <item.icon
+//                           className="h-6 w-6 text-gray-600 group-hover:text-indigo-600"
+//                           aria-hidden="true"
+//                         />
+//                       </div>
+//                       <div className="flex-auto">
+//                         <a
+//                           href={item.href}
+//                           className="block font-semibold text-gray-900"
+//                         >
+//                           {item.name}
+//                           <span className="absolute inset-0" />
+//                         </a>
+//                         <p className="mt-1 text-gray-600">{item.description}</p>
+//                       </div>
+//                     </div>
+//                   ))}
+//                 </div>
+//                 <div className="grid grid-cols-2 divide-x divide-gray-900/5 bg-gray-50">
+//                   {callsToAction.map((item) => (
+//                     <a
+//                       key={item.name}
+//                       href={item.href}
+//                       className="flex items-center justify-center gap-x-2.5 p-3 text-sm font-semibold leading-6 text-gray-900 hover:bg-gray-100"
+//                     >
+//                       <item.icon
+//                         className="h-5 w-5 flex-none text-gray-400"
+//                         aria-hidden="true"
+//                       />
+//                       {item.name}
+//                     </a>
+//                   ))}
+//                 </div>
+//               </Popover.Panel>
+//             </Transition>
+//           </Popover>
+
+//           <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+//             Features
+//           </a>
+//           <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+//             Marketplace
+//           </a>
+//           <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+//             Company
+//           </a>
+//         </Popover.Group>
+//         <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+//           <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+//             Log in <span aria-hidden="true">&rarr;</span>
+//           </a>
+//         </div>
+//       </nav>
+//       <Dialog
+//         as="div"
+//         className="lg:hidden"
+//         open={mobileMenuOpen}
+//         onClose={setMobileMenuOpen}
+//       >
+//         <div className="fixed inset-0 z-10" />
+//         <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+//           <div className="flex items-center justify-between">
+//             <a href="#" className="-m-1.5 p-1.5">
+//               <span className="sr-only">Your Company</span>
+//               <img
+//                 className="h-8 w-auto"
+//                 src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+//                 alt=""
+//               />
+//             </a>
+//             <button
+//               type="button"
+//               className="-m-2.5 rounded-md p-2.5 text-gray-700"
+//               onClick={() => setMobileMenuOpen(false)}
+//             >
+//               <span className="sr-only">Close menu</span>
+//               <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+//             </button>
+//           </div>
+//           <div className="mt-6 flow-root">
+//             <div className="-my-6 divide-y divide-gray-500/10">
+//               <div className="space-y-2 py-6">
+//                 <Disclosure as="div" className="-mx-3">
+//                   {({ open }) => (
+//                     <>
+//                       <Disclosure.Button className="flex w-full items-center justify-between rounded-lg py-2 pl-3 pr-3.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
+//                         Product
+//                         <ChevronDownIcon
+//                           className={classNames(
+//                             open ? "rotate-180" : "",
+//                             "h-5 w-5 flex-none"
+//                           )}
+//                           aria-hidden="true"
+//                         />
+//                       </Disclosure.Button>
+//                       <Disclosure.Panel className="mt-2 space-y-2">
+//                         {[...products, ...callsToAction].map((item) => (
+//                           <Disclosure.Button
+//                             key={item.name}
+//                             as="a"
+//                             href={item.href}
+//                             className="block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+//                           >
+//                             {item.name}
+//                           </Disclosure.Button>
+//                         ))}
+//                       </Disclosure.Panel>
+//                     </>
+//                   )}
+//                 </Disclosure>
+//                 <a
+//                   href="#"
+//                   className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+//                 >
+//                   Features
+//                 </a>
+//                 <a
+//                   href="#"
+//                   className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+//                 >
+//                   Marketplace
+//                 </a>
+//                 <a
+//                   href="#"
+//                   className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+//                 >
+//                   Company
+//                 </a>
+//               </div>
+//               <div className="py-6">
+//                 <a
+//                   href="#"
+//                   className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+//                 >
+//                   Log in
+//                 </a>
+//               </div>
+//             </div>
+//           </div>
+//         </Dialog.Panel>
+//       </Dialog>
+//     </header>
+//   );
+// }
